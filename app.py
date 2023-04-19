b3263984aa2f48b9089473526f9e890abd895fa3
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2023-04-19 22:18:26 +0200

    f

--- app.py
+++ app.py
@@ -11,37 +11,60 @@ def equalizer():
 
 timelist=[]
 
-def equalize_freq(freq_list):
-    for i in freq_list:
+def equalize_freq(amplist,time,f,equalizedmag):
+        fft_amp = np.fft.fft(amplist) # FFT of signal
+        freqs = np.fft.fftfreq(len(amplist), d=time/len(amplist)) # Frequency array
+        freq_mask = (freqs >= f-440) & (freqs <= f)
+        freqs_picked = freqs[freq_mask]
+        new_mag = fft_amp[freqs_picked] * float(equalizedmag)
+        fft_amp[freqs_picked] = new_mag
+        modified_sig = np.fft.ifft(fft_amp) # Inverse FFT of modified FFT
+        modified_sig=np.real(modified_sig)
+        # modified_signal=modified_sig.tolist()
+        return modified_sig
+
+        
+
+
+        
+        
         
 
 @app.route('/calculate-equalized_sig', methods=['POST'])
 def calculate_equalized():
     array= request.get_json()
-    mag_change=array[2]
-    freqadjusted=array[3]
+    mag_change=np.copy(array[2])
+    freqadjusted=np.copy(array[3])
     amp=np.copy(array[1])
-    timelist=array[0].copy()
-    # timesig=np.copy(array[0])
     sigtime=array[0][len(array[0])-2]
 
+    #timelist=array[0].copy()
+    # timesig=np.copy(array[0])
     # Perform FFT
-    fft_amp = np.fft.fft(amp) # FFT of signal
-    freqs = np.fft.fftfreq(len(amp), d=sigtime/len(amp)) # Frequency array
+    # fft_amp = np.fft.fft(amp) # FFT of signal
+    # freqs = np.fft.fftfreq(len(amp), d=sigtime/len(amp)) # Frequency array
     
-    f_index = np.abs(freqs - freqadjusted).argmin() #difference betwween element and freq==0
+    # f_index = np.abs(freqs - freqadjusted).argmin() #difference betwween element and freq==0
     
-    mag = np.abs(fft_amp[f_index])
+    # mag = np.abs(fft_amp[f_index])
     
-    new_mag = fft_amp[f_index] * float(mag_change)
+    # new_mag = fft_amp[f_index] * float(mag_change)
 
-    # Apply new magnitude to FFT
-    fft_amp[f_index] = new_mag
+    # # Apply new magnitude to FFT
+    # fft_amp[f_index] = new_mag
     
-    modified_sig = np.fft.ifft(fft_amp) # Inverse FFT of modified FFT
+    # modified_sig = np.fft.ifft(fft_amp) # Inverse FFT of modified FFT
     
-    modified_sig=np.real(modified_sig)
-    modified_signal=modified_sig.tolist()
+    # modified_sig=np.real(modified_sig)
+    # modified_signal=modified_sig.tolist()
+    
+    for i in range(len(freqadjusted)):
+        amp=equalize_freq(amp,sigtime,freqadjusted[i],mag_change[i])
+        
+        
+        
+    modified_signal=amp.tolist()
+    
     global amplitudelist
     amplitudelist = modified_signal.copy()
      
