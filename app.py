503c68ece110926ba52125bce38fb436375087af
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2023-04-22 14:05:51 +0200

    spectgram etraasam

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template,request, jsonify, send_file,session
 import numpy as np
 import os
+import scipy.signal as signal
+import matplotlib.pyplot as plt
 from scipy.fft import fft, rfft
 from scipy.fft import fftfreq, rfftfreq
 import wave
@@ -12,7 +14,6 @@ app.secret_key = 'your_secret_key'
 def equalizer():
     return render_template('main.html')
 
-timelist=[]
 
 def equalize_freq(amplist,time,f,equalizedmag):
         fft_amp = np.fft.fft(amplist) # FFT of signal
@@ -27,7 +28,17 @@ def equalize_freq(amplist,time,f,equalizedmag):
         return modified_sig
 
         
+@app.route('/send_signal', methods=['POST'])
+def get_signal():
+    array= request.get_json()
+    global originatime 
+    originatime=array[0]
+    global originalamp
+    originalamp=array[1]
+    return jsonify({'sig': originalamp})
 
+
+    
 
         
         
@@ -69,7 +80,7 @@ def calculate_equalized():
     
     global amplitudelist
     amplitudelist = modified_signal.copy()
-     
+    
      
     return jsonify({'equalized_sig': modified_signal})
 
@@ -77,9 +88,8 @@ def calculate_equalized():
 @app.route('/generate_audio')
 def generate_audio():
     
-    file_path = "audio_file.wav"
+    file_path = "spectrogram.png"
     if os.path.exists(file_path):
-        print("زبيٍ")
         os.remove(file_path)
    
     
@@ -112,6 +122,31 @@ def generate_frequency():
     freq = freq.tolist()
 
     return jsonify({'freq': freq, 'mag': mag})
+
+
+@app.route('/spectogram')
+def get_image():
+    file_path = "spectrogram.png"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    oamp=np.copy(originalamp)
+    otime=np.copy(originatime)
+    window = signal.windows.hann(1024)
+    noverlap = 512
+    # Generate a spectrogram using the signal
+    freq, time, Sxx = signal.spectrogram(oamp, fs=1.0/(otime[1]-otime[0]),
+                                        window=window, noverlap=noverlap)
+    plt.pcolormesh(time, freq, 10*np.log10(Sxx), cmap='viridis')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Frequency [Hz]')
+    plt.colorbar()
+
+    # Save the plot as an image
+    plt.savefig('spectrogram.png')
+    filename='spectrogram.png'
+    return send_file(filename, mimetype='image/png')
+
+
 
 
 if __name__ == '__main__':
--- static/js/logic.js
+++ static/js/logic.js
@@ -103,7 +103,15 @@ inputElement.addEventListener('change', (event) => {
 
         // Use timeArray and amplitudeArray for further processing
         // console.log('Time array:', timeArray);
-        console.log('Amplitude array:', amplitudeArray);
+        // console.log('Amplitude array:', amplitudeArray);
+        // const spic = document.getElementById('spectogram');
+      
+        const img = document.getElementById('img');
+        img.setAttribute('src', '');
+
+        sendsig();
+        img.src = '/spectogram';
+        // spic.appendChild(img);
         const trace={
           x: timeArray,
           y: amplitudeArray,name:"original",
@@ -114,23 +122,8 @@ inputElement.addEventListener('change', (event) => {
           },
         };
         Plotly.newPlot(plotDiv, [trace], layout, config);
-        // console.log(amplitudeArray)
 
-        //  get_freq(amplitudeArray)
-        // .then(data => {
-        //   let freq = data.freq;
-        //   let amp = data.amp;
-        //   console.log("Frequency: ", freq);
-        //   console.log("Magnitude: ", amp);
-
-        // //   plotSpectrogram('Specto', freq, timeArray, amp, plot1)
-
-
-
-        // })
-        // .catch(error => {
-        //   console.error(error);
-        // });
+        
       })
       .catch(error => {
         console.error('Error decoding audio data:', error);
@@ -245,56 +238,26 @@ audioElement.setAttribute("src", "/generate_audio");
 
 
 
-const sp = document.getElementById('specto');
 
 
-sp.onclick = async ()=>{
-
-  
-    // get the input file element from the HTML document
-    const fileInput = document.querySelector('input[type="file"]');
-    
-    // create an instance of the AudioContext class
-    const audioContext = new AudioContext();
-    
-    // load the WAV file using the XMLHttpRequest API
-    const fileReader = new FileReader();
-    fileReader.onload = function() {
-      const arrayBuffer = this.result;
-      audioContext.decodeAudioData(arrayBuffer, function(decodedData) {
-        // create an AnalyserNode to analyze the frequency content of the audio data
-        const analyser = audioContext.createAnalyser();
-        analyser.fftSize = 2048;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        analyser.getByteTimeDomainData(dataArray);
-    
-        // create a canvas element to display the spectrogram
-        const canvas = document.createElement('canvas');
-        const canvasContext = canvas.getContext('2d');
-        canvas.width = 800;
-        canvas.height = 300;
-        document.body.appendChild(canvas);
-    
-        // draw the spectrogram on the canvas element
-        function renderFrame() {
-          requestAnimationFrame(renderFrame);
-          analyser.getByteFrequencyData(dataArray);
-          canvasContext.fillStyle = 'rgb(0, 0, 0)';
-          canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-          const barWidth = canvas.width / bufferLength;
-          let barHeight;
-          let x = 0;
-          for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
-            canvasContext.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-            x += barWidth + 1;
-          }
+  function sendsig(callback){
+    let array = [timeArray, amplitudeArray];
+    $.ajax({
+      type: "POST",
+      url: "/send_signal",
+      async: false,
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(array),
+      dataType: "json",
+      success: function(data) {
+        console.log('signal array:',data.sig);
+        
+        if (typeof callback === "function") {
+          callback();
         }
-        renderFrame();
-      });
-    }
-    fileInput.addEventListener('change', function() {
-      fileReader.readAsArrayBuffer(this.files[0]);
-    });}
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
+    });
+  }
